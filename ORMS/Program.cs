501981665a400f01a0a093efbfbf0b4bab4d2cd4
Lili <lili.berenyi@gmail.com>
2024-12-14 15:12:37 +0000

    Add query to list all toys with the dog that owns them

--- ORMS/Program.cs
+++ ORMS/Program.cs
@@ -1,4 +1,5 @@
-﻿using ORMS;
+﻿using Microsoft.EntityFrameworkCore;
+using ORMS;
 
 var toys = Utils.DeserializeFromFile<List<Toy>>("./Resources/Toys.json");
 var dogs = Utils.DeserializeFromFile<List<Dog>>("./Resources/Dogs.json");
@@ -7,16 +8,21 @@ var dogs = Utils.DeserializeFromFile<List<Dog>>("./Resources/Dogs.json");
 
 using (MyDBContext db = new MyDBContext())
 {
-    db.Database.EnsureDeleted();
-    db.Database.EnsureCreated();
+    //db.Database.EnsureDeleted();
+    //db.Database.EnsureCreated();
 
-    foreach (var dog in dogs)
-    {
-        db.Dogs.Add(dog);
-    }
-    foreach (var toy in toys)
-    {
-        db.Toys.Add(toy);
-    }
-    db.SaveChanges();
+    //foreach (var dog in dogs)
+    //{
+    //    db.Dogs.Add(dog);
+    //}
+    //foreach (var toy in toys)
+    //{
+    //    db.Toys.Add(toy);
+    //}
+    //db.SaveChanges();
+
+    var toyQuery = db.Toys.Include(t => t.Dog).ToList();
+    Console.WriteLine($"{"Toy",-20} {"Owner",-20}");
+    Console.WriteLine(new string('=', 41));
+    toyQuery.ForEach(toy => Console.WriteLine($"{toy.Name,-20} {toy.Dog.Name,-20}"));
 }
