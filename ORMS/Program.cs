58abba6ddff81a881828c7915cb32730a358f087
Lili <lili.berenyi@gmail.com>
2024-12-14 14:47:57 +0000

    Populate database

--- ORMS/Program.cs
+++ ORMS/Program.cs
@@ -4,3 +4,19 @@ var toys = Utils.DeserializeFromFile<List<Toy>>("./Resources/Toys.json");
 var dogs = Utils.DeserializeFromFile<List<Dog>>("./Resources/Dogs.json");
 //var parks = Utils.DeserializeFromFile<List<Park>>("./Resources/Parks.json");
 //var dogParkVisits = Utils.DeserializeFromFile<List<DogPark>>("./Resources/DogPark.json");
+
+using (MyDBContext db = new MyDBContext())
+{
+    db.Database.EnsureDeleted();
+    db.Database.EnsureCreated();
+
+    foreach (var dog in dogs)
+    {
+        db.Dogs.Add(dog);
+    }
+    foreach (var toy in toys)
+    {
+        db.Toys.Add(toy);
+    }
+    db.SaveChanges();
+}
